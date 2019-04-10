5cffc6c448bb765dfd7a9a4db2a2a6f50bcb0d0e
Kousilla <rkarpov90@gmail.com>
2019-04-10 16:32:44 -0400

    implement iex api util functions

---
+++ frontend/util/iex_api_util.js
@@ -0,0 +1,29 @@
+const baseUrl = `https://api.iextrading.com/1.0`;
+
+export const fetchStocks = () => {
+  return $.ajax({
+    method: "GET",
+    url: baseUrl + `/ref-data/symbols`
+  })
+}
+
+export const fetchStockQuote = (tickerSymbol) => {
+  return $.ajax({
+    method: "GET",
+    url: baseUrl + `/stock/${tickerSymbol}/quote`
+  })
+}
+
+export const fetchStockCompany = (tickerSymbol) => {
+  return $.ajax({
+    method: "GET",
+    url: baseUrl + `/stock/${tickerSymbol}/company`
+  })
+}
+
+export const fetchStockPrice = (tickerSymbol) => {
+  return $.ajax({
+    method: "GET",
+    url: baseUrl + `/stock/${tickerSymbol}/price`
+  })
+}
