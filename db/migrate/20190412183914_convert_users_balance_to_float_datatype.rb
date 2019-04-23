e8fe16ad364cbf922be19d4af6032d5bb7dbed1e
Kousilla <rkarpov90@gmail.com>
2019-04-23 17:48:15 -0400

    remove migration to push to heroku

--- db/migrate/20190412183914_convert_users_balance_to_float_datatype.rb
+++
@@ -1,8 +0,0 @@
-# class ConvertUsersBalanceToFloatDatatype < ActiveRecord::Migration[5.2]
-#   def change
-#     remove_column :users, :balance, :float
-#     remove_column :transactions, :stock_price, :float
-#     add_column :users, :balance, :decimal, :scale => 2, default: 5000.00
-#     add_column :transactions, :stock_price, :decimal, :scale => 2, null: false
-#   end
-# end
