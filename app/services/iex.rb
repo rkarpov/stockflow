d27d5ecdc00693aae0fbb59185eeaa68b3d0b558
Kousilla <rkarpov90@gmail.com>
2019-06-19 10:12:37 -0400

    revise IEX api calls due to update in api cloud. Add public token for users to make api calls

--- app/services/iex.rb
+++ app/services/iex.rb
@@ -1,22 +1,23 @@
 module IEX
   module API
     def self.base_url
-      return "https://api.iextrading.com"
+      return "https://cloud.iexapis.com"
     end
 
     def self.fetch_quotes(stock_symbols)
-      fetch_quotes = RestClient.get(self.base_url + "/1.0/stock/market/batch?symbols=#{stock_symbols.values.join(',')}&types=quote")
+      fetch_quotes = RestClient.get(self.base_url + "/stable/stock/market/batch?symbols=#{stock_symbols.values.join(',')}&types=quote&token=pk_eb7ec8e475434e668782abf929ddb21e")
       quotes = JSON.parse(fetch_quotes.body)
     end
 
     def self.fetch_price(stock_symbol)
-      fetch_stock_price = RestClient.get(self.base_url + "/1.0/stock/#{stock_symbol}/price")
+      fetch_stock_price = RestClient.get(self.base_url + "/stable/stock/#{stock_symbol}/quote/latestPrice?token=pk_eb7ec8e475434e668782abf929ddb21e")
       fetch_result = JSON.parse(fetch_stock_price.body)
       price = Currency.get_amount(fetch_result) || fetch_result
     end
 
     def self.fetch_open_price(stock_symbol)
-      open_price = RestClient.get(self.base_url + "/1.0/stock/#{stock_symbol}/quote/open")
+      # open_price = RestClient.get(self.base_url + "/stock/#{stock_symbol}/quote/open")
+      open_price = RestClient.get(self.base_url + "/stable/stock/market/batch?symbols=#{stock_symbol}&types=ohlc&token=pk_eb7ec8e475434e668782abf929ddb21e")
       open_price.to_f
     end
   end
